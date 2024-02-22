4ec05363aab30e83478ccf41015a552fc90f1c7e
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-22 17:29:26 +0530

    adding folder named geeksforgeeks

--- aEZeXmftT0CGXl5n7T9A4g_a3dedcb00f6b4586b61ae7a92ff30779_course2_2003291203/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
+++ aEZeXmftT0CGXl5n7T9A4g_a3dedcb00f6b4586b61ae7a92ff30779_course2_2003291203/week1_basic_data_structures/1_brackets_in_code/check_brackets.cpp
@@ -31,15 +31,24 @@ int main() {
         char next = text[position];
 
         if (next == '(' || next == '[' || next == '{') {
-            // Process opening bracket, write your code here
+            opening_brackets_stack.push(Bracket(next, position +1));
         }
 
         if (next == ')' || next == ']' || next == '}') {
-            // Process closing bracket, write your code here
+            if(opening_brackets_stack.empty() || !opening_brackets_stack.top().Matchc(next)){
+                std::cout << position + 1 << std::endl;
+                return 0;
+            }
+            opening_brackets_stack.pop();
         }
     }
+    if (opening_brackets_stack.empty()) {
+        std::cout << "Success" << std::endl;
+    } else {
+        std::cout << opening_brackets_stack.top().position << std::endl;
+    }
 
-    // Printing answer, write your code here
+    
 
     return 0;
 }
