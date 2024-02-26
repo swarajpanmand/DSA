6c66fd8971ad86194bf7d69f7898550037b8518b
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-26 23:40:23 +0530

    binary tree

---
+++ geeksforgeeks/binary tree/inorder_traversal.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        this->data= value;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class BinaryTree{
+public:
+    void inorderTraversal(Node* root){
+        if(root== NULL){
+            return;
+        }
+        inorderTraversal(root->left);
+        cout<<root->data<<" ";
+        inorderTraversal(root->right);
+    }
+
+
+};
+
+int main(){
+    BinaryTree tree;
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    tree.inorderTraversal(root);
+    return 0;
+}
