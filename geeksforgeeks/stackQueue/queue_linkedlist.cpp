5fc0d8e9bd294becefe1108255c36cc11c7f2fd1
Swaraj Panmand <swarajpanmand@gmail.com>
2024-02-29 14:09:37 +0530

    queue

---
+++ geeksforgeeks/stackQueue/queue_linkedlist.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int value){
+        this->data = value;
+        next = NULL;
+    }
+};
+
+class Queue{
+private:
+    Node* front;
+    Node* rear;
+
+public:
+   Queue(){
+    front = rear = NULL;
+   }
+
+   void enqueue(int value){
+        Node* newNode = new Node(value);
+        if(isEmpty()){
+            front = rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
+   }
+
+   void dequeue(){
+    if(isEmpty()){
+        cout<<"Queue Underflow"<<endl;
+        return;
+    }
+    Node* temp = front;
+    front = front->next;
+    if(front == NULL){
+        rear = NULL;
+    }
+    delete temp;
+   }
+
+   int peek(){
+    if(isEmpty()){
+        cout<<"Queue is empty"<<endl;
+        return -1;
+    }
+    return front->data;
+   }
+
+   bool isEmpty(){
+        return front == NULL;
+
+   }
+};
+
+int main(){
+    Queue myQueue;
+
+    myQueue.enqueue(1);
+    myQueue.enqueue(2);
+    myQueue.enqueue(3);
+
+    cout<<"Front element: "<<myQueue.peek()<<endl;
+
+    myQueue.dequeue();
+    myQueue.dequeue();
+
+    cout<<"Is the Queue empty? "<<(myQueue.isEmpty() ? "Yes" : "No")<<endl;
+
+    return 0;
+}
